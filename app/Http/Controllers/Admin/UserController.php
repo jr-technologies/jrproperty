ef8001fc2c86dad5e5d1ce970aa43675a21d5c90
waqas <jrproperty167@gmail.com>
2016-03-10 09:19:03 +0500

    some change happend in this user

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -24,7 +24,7 @@ class UserController extends AdminController
     {
         $staffs = User::all();
         $heading = 'List of Staff';
-        return view('staff.listing', compact('staffs', 'heading'))->with('section', $this->section);
+        return view('staff.listing',compact('staffs', 'heading'))->with('section', $this->section);
     }
 
     /**
